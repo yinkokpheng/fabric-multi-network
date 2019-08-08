6f334582d2536861a4378727eddadb962dc5385f
kokpheng <yinkokpheng@gmail.com>
2019-08-09 00:10:23 +0900

    Add start script

--- start.sh
+++ start.sh
@@ -1,12 +1,16 @@
-./bymn.sh generate crypto-config.yaml
-
-scp -r crypto-config channel-artifacts docker-compose-org1.yaml kp@worker1:~/blockchain-project/fabric-multi-network
-scp -r crypto-config channel-artifacts docker-compose-org2.yaml kp@worker2:~/blockchain-project/fabric-multi-network
-
-scp -r bymn.sh kp@worker1:~/blockchain-project/fabric-multi-network
-scp -r bymn.sh kp@worker2:~/blockchain-project/fabric-multi-network
-
 docker network create --attachable --driver overlay fabric
 
+./bymn.sh generate crypto-config.yaml
+
+USER=kp
+SERVER_HOST_1=worker1
+SERVER_HOST_2=worker2
+PROJECT_PATH=~/blockchain-project/fabric-multi-network
+
+scp -r bymn.sh crypto-config channel-artifacts docker-compose-org1.yaml $USER@$SERVER_HOST_1:$PROJECT_PATH
+scp -r bymn.sh crypto-config channel-artifacts docker-compose-org2.yaml $USER@$SERVER_HOST_2:$PROJECT_PATH
 
 yes | ./bymn.sh down && yes | docker volume prune
+
+
+yes | ./byfn.sh down && yes | docker volume prune
